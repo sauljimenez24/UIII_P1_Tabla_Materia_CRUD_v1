81b5646e136e2f92c1bfddee33da8fa5f3716441
sauljimenez24 <a.22308051281222@cbtis128.edu.mx>
2024-11-06 08:51:04 -0600

    Add files via upload

---
+++ UIII_P1_Tabla_Materia_CRUD_v2/materia_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Materia
+
+# Create your views here.
+
+def inicio_vista(request):
+    lasmaterias=Materia.objects.all()
+    return render(request,"gestionarMateria.html",{"mismaterias":lasmaterias})
